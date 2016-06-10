113b7a3b3fc309fa98867d17629d197c6aeeb456
Innokentiy Mazhara <keshan3262@gmail.com>
2016-06-10 11:24:29 +0300

    Delete certificate.js

--- reestr/models/certificate.js
+++
@@ -1,9 +0,0 @@
-var mongoose	= require('mongoose');
-var Schema		= mongoose.Schema;
-
-var CertificateSchema = new Schema({
-	legal_entity_id : Schema.Types.ObjectId,
-	certified_date: Date
-});
-
-module.exports = mongoose.model('Certificate', CertificateSchema);
