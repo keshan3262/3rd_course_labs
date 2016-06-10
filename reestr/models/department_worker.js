6ba77e72034d862559d8d07855c2df7a64c74071
Innokentiy Mazhara <keshan3262@gmail.com>
2016-06-10 11:26:47 +0300

    Delete department_worker.js

--- reestr/models/department_worker.js
+++
@@ -1,9 +0,0 @@
-var mongoose	= require('mongoose');
-var Schema		= mongoose.Schema;
-
-var DepartmentWorkerSchema = new Schema({
-	name: String, surname: String, fathername: String,
-	address: String, fired_out: Boolean
-});
-
-module.exports = mongoose.model('DepartmentWorker', DepartmentWorkerSchema);
